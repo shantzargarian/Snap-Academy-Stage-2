1df61e53a8bb8ecfd494f3b75434485d20176df6
shantzargarian <shant.zargarian@gmail.com>
2024-04-09 20:58:54 -0700

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,70 +1,137 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
-
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+let carData = [
+    {
+        title: "CarChives Car Group",
+        imageUrl: "images/4Cars.jpg",
+        videoId: "7240638788169370923",
+        coordinates: { lat: 34.4344907, lng: -118.56198293046876 }
+    },
+    {
+        title: "Porsche 911 S/T",
+        imageUrl: "images/911ST.jpg",
+        videoId: "7324117687192292638",
+        coordinates: { lat: 34.10785480386952, lng: -118.25617381240207 }
+    },
+    {
+        title: "Porsche GT4",
+        imageUrl: "images/GT4.jpg",
+        videoId: "7339651796702448926", 
+        coordinates: { lat: 34.109513, lng: -118.788448 }
+    },
+    {
+        title: "Vorsteiner E46 M3", 
+        imageUrl: "images/VorsteinerE46.jpg",
+        videoId: "7339620446746299679",
+        coordinates: { lat: 34.058947, lng: -118.806641 }
+    },
+    {
+        title: "Ferrari 488 Pista",
+        imageUrl: "images/488pista.jpg",
+        videoId: "7238325766117788970",
+        coordinates: { lat: 34.10785480386952, lng: -118.25617381240207 }
+    },
+    {
+        title: "370z and M235i",
+        imageUrl: "images/370x235.jpg",
+        videoId: "7235801943417900330",
+        coordinates: { lat: 34.052351, lng: -118.251854 }
+    },
+    {
+        title: "Porsche GT3's",
+        imageUrl: "images/blackGT3s.jpg",
+        videoId: "7324082865635904799",
+        coordinates: { lat: 34.10785480386952, lng: -118.25617381240207 }
+    },
+    {
+        title: "RWB Porsche 911",
+        imageUrl: "images/rwb911.jpg",
+        videoId: "7196392086466350382",
+        coordinates: { lat: 34.417439, lng: -118.548375 }
+    },
+    {
+    title: "Ferrari & Lamborghini", 
+        imageUrl: "images/lamboRari.jpg", 
+        videoId: "7191182826660695342",
+        coordinates: { lat: 34.202436, lng: -118.191935 }
+    },
+    {
+        title: "Ford Focus RS",
+        imageUrl: "images/focusRS.jpg",
+        videoId: "7221393744103361835",
+        coordinates: { lat: 34.206625, lng: -118.217529 }
+    },
+    {
+        title: "iND G87 M2",
+        imageUrl: "images/indM2.jpg",
+        videoId: "7275383527506578719",
+        coordinates: { lat: 34.262007, lng: -118.112905 } 
+    },
+    {
+        title: "370z",
+        imageUrl: "images/370z.jpg",
+        videoId: "7317032072961412383",
+        coordinates: { lat: 34.096030, lng:-118.409429 }
+    }
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
+
+document.addEventListener("DOMContentLoaded", function() {
+    showCards();
+
+    const searchInput = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+
+    function filterCards() {
+        const inputText = searchInput.value.toUpperCase();
+        const cards = document.querySelectorAll('.card');
+
+        cards.forEach(card => {
+            const title = card.querySelector("h2").textContent.toUpperCase();
+            if (title.indexOf(inputText) > -1) {
+                card.style.display = "";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    }
+
+    searchButton.addEventListener("click", filterCards);
+
+    searchInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            filterCards();
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleViewButton = document.getElementById('toggleViewButton');
+    const cardContainer = document.getElementById('card-container');
+
+    toggleViewButton.addEventListener('click', function() {
+        cardContainer.classList.toggle('list-view');
+
+        // Update the button text based on the current view
+        if (cardContainer.classList.contains('list-view')) {
+            toggleViewButton.textContent = 'Grid View';
+        } else {
+            toggleViewButton.textContent = 'List View';
+        }
+    });
+});
 
 
-// This function adds cards the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+    carData.forEach(car => {
+        const nextCard = templateCard.cloneNode(true);
+        editCardContent(nextCard, car.title, car.imageUrl, car.videoId, car.coordinates);
+        cardContainer.appendChild(nextCard);
+    });
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, newTitle, newImageURL, videoID, mapCoords) {
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
@@ -72,23 +139,56 @@ function editCardContent(card, newTitle, newImageURL) {
 
     const cardImage = card.querySelector("img");
     cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
+    cardImage.alt = `${newTitle} Poster`;
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+    const viewVideoButton = card.querySelector(".button-style.tiktok");
+    viewVideoButton.onclick = () => toggleTikTokEmbed(videoID);
+
+    const viewMapButton = card.querySelector(".button-style.map");
+    viewMapButton.onclick = () => toggleGoogleMapsEmbed(mapCoords.lat, mapCoords.lng);
 }
 
-// This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function toggleTikTokEmbed(videoID) {
+    const videoModal = document.getElementById('videoModal');
+    const tiktokVideoFrame = document.getElementById('tiktokVideoFrame');
+
+    if (videoModal.style.display !== "block" && videoID) {
+        tiktokVideoFrame.src = `https://www.tiktok.com/embed/v2/${videoID}?lang=en-US`;
+        videoModal.style.display = 'block';
+    } else {
+        videoModal.style.display = 'none';
+        tiktokVideoFrame.src = '';
+    }
 }
 
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+function toggleGoogleMapsEmbed(lat, lng) {
+    const mapsModal = document.getElementById('mapModal');
+    const mapDisplayFrame = document.getElementById('googleMapsFrame');
+
+    if (mapsModal.style.display === "none" && lat && lng) {
+        mapDisplayFrame.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyCqRJzyUarVDlXrHa_m7Naq8jaM7pt6mm8
+            &q=${encodeURIComponent(lat + ',' + lng)}&zoom=14`;
+        mapsModal.style.display = "block";
+    } else {
+        mapsModal.style.display = 'none';
+        mapDisplayFrame.src = '';
+    }
+}
+
+function closeTiktokModal() {
+    const videoModal = document.getElementById('videoModal');
+    const tiktokVideoFrame = document.getElementById('tiktokVideoFrame');
+
+    videoModal.style.display = 'none';
+    tiktokVideoFrame.src = '';
+}
+
+function closeMapModal() {
+    const mapsModal = document.getElementById('mapModal');
+    const mapDisplayFrame = document.getElementById('googleMapsFrame');
+
+    mapsModal.style.display = 'none';
+    mapDisplayFrame.src = '';
 }
